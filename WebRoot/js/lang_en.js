672abeac39d1c8e05834e33dfa8b50ce03a8ab0a
s0293 <rafaeljin1994@gmail.com>
2016-05-17 10:52:53 +0800

    clear

--- WebRoot/js/lang_en.js
+++
@@ -1,8 +0,0 @@
-RLab.Lang = {
-	digitalLogic : 'Digital Logic',
-	principleofComputer : 'Principle of Computer',
-	computerArchitecture : 'Computer Architecture',
-	dlexp1 : 'Experiment 1',
-	dlexp2 : 'Experiment 2',
-	dlexp3 : 'Experiment 3'
-};
